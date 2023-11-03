e53b235772898ab871cc0eba2b8608b847774f64
otabekmbw124 <44807498+otabekmbw124@users.noreply.github.com>
2023-11-04 01:15:20 +0500

    Add files via upload

---
+++ lesson/intro-76/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Otabek';
+const lastName = 'Rakhmankulov';
+let age = '26';
+let cityName = 'Perm';
+const fromCountry = 'Kyrgyzstan';
+
+console.log(firstName, lastName, age, 'y.o', cityName, fromCountry)
